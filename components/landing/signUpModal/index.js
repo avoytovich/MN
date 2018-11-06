33d0cb4bc05b02cbd2561892655343a1651f9e4f
Daniel Dowd <tanto@Daniels-MacBook-Pro-2.local>
2018-11-06 14:05:19 -0600

    Added new address to bottom of page

--- components/landing/signUpModal/index.js
+++ components/landing/signUpModal/index.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react'
-import { withStyles, Avatar, Typography, Button ,TextField} from '@material-ui/core';
-import { Formik, Form, Field , withFormik } from 'formik';
-import { signUp } from 'actions/account'
-import { toggleSnackbar } from 'actions/snackbar'
+import React, { Component } from 'react';
+import {
+  withStyles,
+  Avatar,
+  Typography,
+  Button,
+  TextField
+} from '@material-ui/core';
+import { Formik, Form, Field, withFormik } from 'formik';
+import { signUp } from 'actions/account';
+import { toggleSnackbar } from 'actions/snackbar';
 import CloseIcon from '@material-ui/icons/Close';
 import * as Yup from 'yup';
 import { connect } from 'react-redux';
-import Router from 'next/router'
+import Router from 'next/router';
 
-import style from '../styles'
+import style from '../styles';
 
-const styles = theme => ( style );
-
+const styles = theme => style;
 
 @connect(
   null,
@@ -28,33 +33,34 @@ const styles = theme => ( style );
       .min(6, 'Password should be at least 6 symbols')
       .required('Required'),
     ConfirmPassword: Yup.string()
-      .oneOf([Yup.ref('Password'), null],'Confirm password field doesn’t match password')
+      .oneOf(
+        [Yup.ref('Password'), null],
+        'Confirm password field doesn’t match password'
+      )
       .required('Required')
   }),
   handleSubmit: async (values, { props }) => {
-
     try {
-      await signUp(values)
+      await signUp(values);
       Router.push({
         pathname: '/home/manage-groups'
-      })
+      });
     } catch (e) {
-      const  { message} = e.response.data.errors[0]
-      props.toggleSnackbar(message, 'error')
+      const { message } = e.response.data.errors[0];
+      props.toggleSnackbar(message, 'error');
     }
-
   }
 })
 @withStyles(styles)
 export default class IconModal extends Component {
-
-
   render() {
-    const { classes, close ,errors} = this.props;
+    const { classes, close, errors } = this.props;
     return (
       <div className={classes.wrap}>
-        <CloseIcon onClick={() => close()}/>
-        <Typography align="center" className={classes.title}>Sign Up</Typography>
+        <CloseIcon onClick={() => close()} />
+        <Typography align="center" className={classes.title}>
+          Sign Up
+        </Typography>
         <Form>
           <TextField
             className={classes.input}
@@ -63,7 +69,7 @@ export default class IconModal extends Component {
             error={this.props.errors.OrganizationId !== undefined}
             type="text"
             name="OrganizationId"
-            placeholder="Group name"
+            placeholder="OrganizationId"
             fullWidth
             margin="normal"
           />
@@ -74,7 +80,7 @@ export default class IconModal extends Component {
             error={this.props.errors.Email !== undefined}
             type="email"
             name="Email"
-            placeholder="Group name"
+            placeholder="Email"
             fullWidth
             margin="normal"
           />
@@ -85,7 +91,7 @@ export default class IconModal extends Component {
             error={this.props.errors.Password !== undefined}
             type="password"
             name="Password"
-            placeholder="Group name"
+            placeholder="Password"
             fullWidth
             margin="normal"
           />
@@ -96,17 +102,21 @@ export default class IconModal extends Component {
             error={this.props.errors.ConfirmPassword !== undefined}
             type="password"
             name="ConfirmPassword"
-            placeholder="Group name"
+            placeholder="Confirm Password"
             fullWidth
             margin="normal"
           />
-          <Button type="submit"  className={classes.submit}>Sign Up</Button>
-          <Typography align="center" className={classes.haveNotAccount}>Already have an account?
-            <Typography component="a" className={classes.signUp}>Log In</Typography>
-
+          <Button type="submit" className={classes.submit}>
+            Sign Up
+          </Button>
+          <Typography align="center" className={classes.haveNotAccount}>
+            Already have an account?
+            <Typography component="a" className={classes.signUp}>
+              Log In
+            </Typography>
           </Typography>
         </Form>
       </div>
-    )
+    );
   }
 }
