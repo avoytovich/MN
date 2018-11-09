f85eb6311aeceaaa9fd11d394112253d86a863e5
Andrii <andrii.voitovych@kindgeek.com>
2018-11-09 11:58:39 +0200

    1. Create dynamic pages 2. Create route group page 3. Clean urls with route masking 4. Server side support for clean urls
    5. Separate underheader component
    6. Render dynamic data

---
+++ components/underheader/index.js
@@ -0,0 +1,55 @@
+import { Component, Fragment } from 'react';
+import { withRouter } from 'next/router';
+import SecondPanel from 'components/secondpanel';
+import { Button } from '@material-ui/core';
+
+@withRouter
+export class Group extends Component {
+
+  titleCase = (str) => {
+    if (str.search('-') !== -1) {
+      const splitStr = str.split('-');
+      splitStr.forEach((item, index) => {
+        splitStr[index] = splitStr[index].charAt(0).toUpperCase() + splitStr[index].substring(1);
+      });
+      return splitStr.join(' ');
+    }
+    const splitStr = str.toLowerCase().split(' ');
+    splitStr.forEach((item, index) => {
+      splitStr[index] = splitStr[index].charAt(0).toUpperCase() + splitStr[index].substring(1);
+    });
+    return splitStr.join(' ');
+  };
+
+  titlePath = (str) => {
+    const splitStr = str.toLowerCase().split('/');
+    const path = splitStr[splitStr.length - 2].replace('-', ' ');
+    return path.charAt(0).toUpperCase() + path.substring(1);
+  };
+
+  render() {
+    const {
+      query: { name },
+      pathname
+    } = this.props.router;
+    return (
+      <Fragment>
+        <SecondPanel
+          actionButtons={[
+            <Button variant="outlined" color="secondary"> INVITE
+            </Button>
+          ]}
+          centerButtons={[
+            <Button variant="contained" color="primary">
+              ? Start A QUIZ
+            </Button>
+          ]}
+          breadCrumb={`${this.titlePath(pathname)} / ${this.titleCase(name)}`}
+          title="Group Content"
+        />
+      </Fragment>
+    );
+  }
+}
+
+export default Group;
